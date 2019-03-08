f0f3870eb4b7dcb2958d5a0be6d6250737b3e320
bfchengnuo <bfchengnuo@gmail.com>
2019-03-08 15:24:30 +0800

    feat 城市路由

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@style/varibles.styl'
+  .header
+    position: relative
+    overflow: hidden
+    height: .86rem
+    line-height: .86rem
+    text-align: center
+    color: #ffffff
+    background-color: $bgColor
+    font-size: .32rem
+    .back-icon
+      position: absolute
+      top: 0
+      left: 0
+      text-align: center
+      width: .64rem
+      font-size: .4rem
+      color: #ffffff
+</style>
