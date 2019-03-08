76e95e780f3a2b77b7ca801dd3a8da5d28b040c8
bfchengnuo <bfchengnuo@gmail.com>
2019-03-08 10:23:53 +0800

    feat 推荐部分排版

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+      <div class="recommend-title">热销推荐</div>
+      <ul>
+        <!-- border-bottom 一像素边框-->
+        <li
+          class="item border-bottom"
+          v-for="item of RecommendList"
+          :key="item.id"
+        >
+          <img class="item-img" :src="item.imgUrl" />
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+            <button class="item-button">查看详情</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      RecommendList: [{
+        id: 1,
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201402/25/aafec297c9838bab7c5ba84b1cbaad57.jpg_250x250_c1ae475d.jpg',
+        desc: '描述描述这是描述1',
+        title: '标题这是标题1'
+      }, {
+        id: 2,
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201402/25/aafec297c9838bab7c5ba84b1cbaad57.jpg_250x250_c1ae475d.jpg',
+        desc: '描述描述这是描述2',
+        title: '标题这是标题2'
+      }, {
+        id: 3,
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201402/25/aafec297c9838bab7c5ba84b1cbaad57.jpg_250x250_c1ae475d.jpg',
+        desc: '描述描述这是描述3',
+        title: '标题这是标题3'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~@style/mixins.styl"
+  .recommend-title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <!-- border-bottom 一像素边框-->
+      <li
+        class="border-bottom"
+        v-for="item of WeekendList"
+        :key="item.id"
+      >
+        <div class="img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      WeekendList: [{
+        id: 1,
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/ef/815eec316c47c4.jpg_r_640x214_5bf7f999.jpg',
+        desc: '描述描述这是描述1',
+        title: '标题这是标题1'
+      }, {
+        id: 2,
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/ef/815eec316c47c4.jpg_r_640x214_5bf7f999.jpg',
+        desc: '描述描述这是描述2',
+        title: '标题这是标题2'
+      }, {
+        id: 3,
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/ef/815eec316c47c4.jpg_r_640x214_5bf7f999.jpg',
+        desc: '描述描述这是描述3',
+        title: '标题这是标题3'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~@style/mixins.styl"
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 34%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    min-width: 0
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
