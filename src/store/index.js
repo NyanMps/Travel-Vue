e7d436e3be10b2d50be42c7c05c471b4891c8b3f
bfchengnuo <bfchengnuo@gmail.com>
2019-03-09 09:38:46 +0800

    使用localStorage本地存储选择的城市

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,17 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let defaultCity = '北京'
+try {
+  if (localStorage.city) {
+    defaultCity = localStorage.city
+  }
+} catch (e) {
+}
+
 export default new Vuex.Store({
   state: {
-    city: '北京'
+    city: defaultCity
   },
   actions: {
     changeCity (ctx, name) {
@@ -16,6 +24,11 @@ export default new Vuex.Store({
   mutations: {
     changeCity (state, name) {
       state.city = name
+      // 本地存储 (低版本浏览器或者隐身模式可能会抛异常)
+      try {
+        localStorage.city = name
+      } catch (e) {
+      }
     }
   }
 })
